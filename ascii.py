a451e6576fab36f9b1b9a9bdb1caaf4edcc3056d
Adeel Ahmed <adeel@Adeels-MacBook-Air.local>
2022-07-22 08:58:34 +0100

    small python project

---
+++ ascii.py
@@ -0,0 +1,13 @@
+# Adeel Ahmed
+# Fun fact: I picked up a Lion in Egypt
+
+name = """
+      A.        A
+     A A.       A
+  A       A     A
+   A A A A.     A
+  A        A.   A
+ A          A   A
+A            A. A
+"""
+print(name)
