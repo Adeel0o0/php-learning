eb6b6be4aec14a44e34d18ab17cd07e6e810fb21
Adeel Ahmed <adeel@Adeels-MacBook-Air.local>
2022-08-01 15:51:47 +0100

    drone pricing

--- shipping.py
+++ shipping.py
@@ -14,3 +14,16 @@ print("Ground shipping $", cost_ground_shipping)
 
 cost_premium_shipping = 125.00
 print("Ground shipping Premium $", cost_premium_shipping)
+
+
+#Drone shipping
+if weight <= 2:
+ cost_drone_shipping = weight * 4.5
+elif weight <= 6:
+ cost_drone_shipping = weight * 9
+elif weight <= 10:
+  cost_drone_shipping = weight * 12
+else:
+  cost_drone_shipping = weight * 14.25
+
+print("Drone Shipping $", cost_drone_shipping)
