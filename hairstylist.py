0ffa0ca2b60265085079f8de8ac85fdb030f5030
Adeel Ahmed <adeel@Adeels-MacBook-Air.local>
2022-09-01 22:13:27 +0100

    reduce price by 5

--- hairstylist.py
+++ hairstylist.py
@@ -4,6 +4,7 @@ prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
+#Prices and Cuts
 total_price = 0
 for price in prices:
   total_price += price
@@ -12,3 +13,6 @@ for price in prices:
 
 average_price = total_price / len(prices)
 print("Average Haircut Price:", average_price)
+
+new_prices = [price - 5 for price in prices]
+print(new_prices)
