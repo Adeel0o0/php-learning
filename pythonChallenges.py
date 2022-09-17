991767a84422a983b175ccfc5e13c0130a96e299
Adeel Ahmed <adeel0754@gmail.com>
2022-09-17 22:00:50 +0100

    remove_middle

--- pythonChallenges.py
+++ pythonChallenges.py
@@ -3,3 +3,16 @@
 def every_three_nums(start):
   num_between = [*range(start, 101, 3)]
   return num_between
+
+print(every_three_nums(81))
+
+
+#Remove Middle
+
+def remove_middle(lst, start, end):
+  lst_start = lst[:start] 
+  lst_end = lst[end+1:]
+  return_list = lst_start + lst_end
+  return return_list
+  
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
