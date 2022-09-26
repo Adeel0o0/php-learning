30339ca19d66ec8e3c264a800e05cf3570ad71ee
Adeel Ahmed <adeel0754@gmail.com>
2022-09-26 22:27:32 +0100

    Middle Item Challenge

--- pythonChallenges.py
+++ pythonChallenges.py
@@ -37,3 +37,15 @@ def double_index(lst, index):
         return newList
   
 print(double_index([3, 8, -10, 12], 2))
+
+
+#Middle Item
+
+def middle_element(lst):
+  if len(lst) % 2 == 0:
+    sum = lst[int(len(lst)/2)] + lst[int(len(lst)/2) - 1]
+    return sum / 2
+  else:
+    return lst[int(len(lst)/2)]
+
+print(middle_element([5, 2, -10, -4, 4, 5]))
