b4edf4b92430aefaf31fca7701ff5a73b2b624dc
Adeel Ahmed <adeel0754@gmail.com>
2022-10-22 20:43:23 +0100

    all operations

--- pythonChallenges.py
+++ pythonChallenges.py
@@ -176,3 +176,21 @@ def dog_years(name, age):
 print(dog_years("Lola", 16))
 
 print(dog_years("Baby", 0))
+
+
+#all operations
+
+def lots_of_math(a, b, c, d):
+  first =  a + b
+  second = c -d
+  third = first * second
+  fourth = third % a
+  print(first)
+  print(second)
+  print(third)
+  return fourth
+  
+
+print(lots_of_math(1, 2, 3, 4))
+
+print(lots_of_math(1, 1, 1, 1))
