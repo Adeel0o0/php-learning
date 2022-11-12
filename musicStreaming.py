de9d9ab91afb1a42c74ffddec0ed494a38efbb1d
Adeel Ahmed <adeel0754@gmail.com>
2022-11-12 21:47:42 +0000

    creating a dictionary of two lists

---
+++ musicStreaming.py
@@ -0,0 +1,4 @@
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {key:value for key, value in zip(songs, playcounts)}
