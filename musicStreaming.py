e2ba14408aa6c47f589539ccb23f6fa992a58608
Adeel Ahmed <adeel0754@gmail.com>
2022-11-12 21:51:30 +0000

    another dictionary

--- musicStreaming.py
+++ musicStreaming.py
@@ -4,3 +4,9 @@ playcounts = [78, 29, 44, 21, 89, 5]
 plays = {key:value for key, value in zip(songs, playcounts)}
 
 plays["Purple Haze"] = 1
+
+plays["Respect"] = 94
+
+library = {"The Best Songs": plays, "Sunday Feelings":{}}
+
+print(library)
