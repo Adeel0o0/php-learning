aa6ccc6b8ed6b6f0ddaef69f9b415b9acb09c2bc
Adeel Ahmed <adeel0754@gmail.com>
2022-12-14 22:30:39 +0000

    notify the boss

--- TheFender.py
+++ TheFender.py
@@ -6,4 +6,17 @@ with open('passwords.csv') as password_file:
   password_csv = csv.DictReader(password_file)
   
   for password_row in password_csv:
-    print(password_row['Username'])
+   compromised_users.append(password_row['Username'])
+
+with open('compromised_users.txt', 'w') as compromised_user_file:
+  for compromised_user in compromised_users:
+    compromised_user_file.write(compromised_user)
+
+#notify the boss
+
+with open('boss_message.json','w') as boss_message:
+  boss_message_dict = {
+    'recipient':'The Boss',
+    'message': 'Mission Succes'
+  }
+  json.dump(boss_message_dict, boss_message)
