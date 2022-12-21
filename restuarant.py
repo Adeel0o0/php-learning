0e971d0bfde05eeacf8018e9ca404cc7634914da
Adeel Ahmed <adeel0754@gmail.com>
2022-12-21 09:16:08 +0000

    another menu item

--- restuarant.py
+++ restuarant.py
@@ -9,6 +9,11 @@ brunch_items = {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }
 
-brunch_menu = Menu('Brunch', brunch_items,1100, 1600)
-print(brunch_menu.name)
-print(brunch_menu.items)
+brunch_menu = Menu('Brunch', brunch_items, 1100, 1600)
+
+
+early_bird_items = {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}
+
+early_bird = Menu('Early bird', early_bird_items, 1500, 1800)
