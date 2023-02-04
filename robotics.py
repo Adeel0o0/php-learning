89edc71e8ffe046d06ca924404e2bc289e4c4b58
Adeel Ahmed <adeel0754@gmail.com>
2023-02-04 21:51:51 +0000

    add method to change speed and direction

--- robotics.py
+++ robotics.py
@@ -7,10 +7,18 @@ class DriveBot:
     self.direction = 0
     self.sensor_range = 0
 
+#method to change speed and direction
+  def control_bot(self,new_speed, new_direction):
+      self.motor_speed = new_speed
+      self.direction = new_direction
+
+  def adjust_sensor(self, new_sensor_range):
+        self.sensor_range = new_sensor_range
+
 robot_1 = DriveBot()
-robot_1.motor_speed = 5
-robot_1.direction = 90
-robot_1.sensor_range = 10
+robot_1.motor_speed = 10
+robot_1.direction = 180
+robot_1.sensor_range = 20
 
 print(robot_1.motor_speed)
 print(robot_1.direction)
