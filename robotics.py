a701c0b89f3cc0ff55c9c9eea7a700c9dccbde6a
Adeel Ahmed <adeel0754@gmail.com>
2023-02-04 22:15:41 +0000

    update constructor to take three optional parameters

--- robotics.py
+++ robotics.py
@@ -2,10 +2,10 @@
 #1. Setting Up Our Robot
 
 class DriveBot:
-  def __init__(self):
-    self.motor_speed = 0
-    self.direction = 0
-    self.sensor_range = 0
+  def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
+        self.motor_speed = motor_speed
+        self.direction = direction
+        self.sensor_range = sensor_range
 
 #method to change speed and direction
   def control_bot(self,new_speed, new_direction):
@@ -20,6 +20,13 @@ robot_1.motor_speed = 10
 robot_1.direction = 180
 robot_1.sensor_range = 20
 
-print(robot_1.motor_speed)
-print(robot_1.direction)
-print(robot_1.sensor_range)
+# print(robot_1.motor_speed)
+# print(robot_1.direction)
+# print(robot_1.sensor_range)
+
+robot_2 = DriveBot(35, 75, 25)
+
+print(robot_2.motor_speed)
+print(robot_2.direction)
+print(robot_2.sensor_range)
+
