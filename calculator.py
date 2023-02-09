25340af27e39f5807d508bd6e5c3c55d89333662
Adeel Ahmed <adeel0754@gmail.com>
2023-02-09 19:51:55 +0000

    minus function

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add (a, b):
+    answer = a + b
+    print(str(a) + " + ", str(b), " = " + str(answer))
+
+add (5, 7)
+
+
+def minus (a, b):
+    answer = a - b
+    print(str(a) + " - ", str(b), " = " + str(answer))
+
+minus (12, 2)
