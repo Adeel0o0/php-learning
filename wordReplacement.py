badef297cceb84538223ec921cdb81eb89f1b37c
Adeel Ahmed <adeel0754@gmail.com>
2023-02-09 19:35:53 +0000

    word replacement

---
+++ wordReplacement.py
@@ -0,0 +1,7 @@
+def replace_word():
+    string = "Hi, I am Sam"
+    word_to_replace = input("Enter the word to replace")
+    word_replacement = input("Enter the word replacement")
+    print(string.replace(word_to_replace, word_replacement ))
+
+replace_word()
