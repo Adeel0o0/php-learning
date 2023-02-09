0feb51f3c3c986b88f2dfb9bbcdeaf40a033e4b9
Adeel Ahmed <adeel0754@gmail.com>
2023-02-09 20:16:11 +0000

    while loop to continue program and spacing

--- calculator.py
+++ calculator.py
@@ -1,50 +1,48 @@
-def add (a, b):
+def add(a, b):
     answer = a + b
-    print(str(a) + " + ", str(b), " = " + str(answer))
+    print(str(a) + " + ", str(b), " = " + str(answer) + "\n")
 
-
-def sub (a, b):
+def sub(a, b):
     answer = a - b
-    print(str(a) + " - ", str(b), " = " + str(answer))
-
+    print(str(a) + " - ", str(b), " = " + str(answer) + "\n")
 
 def multiply(a, b):
     answer = a * b
-    print(str(a) + " x ", str(b), " = " + str(answer))
-
+    print(str(a) + " x ", str(b), " = " + str(answer) + "\n")
 
 def divide(a, b):
     answer = a / b
-    print(str(a) + " / ", str(b), " = " + str(answer))
+    print(str(a) + " / ", str(b), " = " + str(answer) + "\n")
 
-print ("A, addition")
-print ("B, subtraction")
-print ("C, multiplication")
-print ("D, division")
-print ("E, exit")
 
-choice = input("Enter your choice: ")
-if choice == "a" or choice == "A":
-    print("Addition")
-    a = int(input("Enter your first number: "))
-    b = int(input("Enter your second number: "))
-    add (a, b)
-elif choice == "b" or choice == "B":
-    print('Substraction')
-    a = int(input("Enter your first number: "))
-    b = int(input("Enter your second number: "))
-    sub (a,b)
-elif choice == "c" or choice == "C":
-    print('Multiplication')
-    a = int(input("Enter your first number: "))
-    b = int(input("Enter your second number: "))
-    multiply (a,b)
-elif choice == "d" or choice == "D":
-    print('Division')
-    a = int(input("Enter your first number: "))
-    b = int(input("Enter your second number:"))
-    multiply (a,b)
-elif choice == "e" or choice == "E":
-    print('Program ended')
-    quit()
-   
+while True:
+    print("A, addition")
+    print("B, subtraction")
+    print("C, multiplication")
+    print("D, division")
+    print("E, exit")
+
+    choice = input("Enter your choice: ")
+    if choice == "a" or choice == "A":
+        print("Addition")
+        a = int(input("Enter your first number: "))
+        b = int(input("Enter your second number: "))
+        add(a, b)
+    elif choice == "b" or choice == "B":
+        print('Substraction')
+        a = int(input("Enter your first number: "))
+        b = int(input("Enter your second number: "))
+        sub(a, b)
+    elif choice == "c" or choice == "C":
+        print('Multiplication')
+        a = int(input("Enter your first number: "))
+        b = int(input("Enter your second number: "))
+        multiply(a, b)
+    elif choice == "d" or choice == "D":
+        print('Division')
+        a = int(input("Enter your first number: "))
+        b = int(input("Enter your second number:"))
+        multiply(a, b)
+    elif choice == "e" or choice == "E":
+        print('Program ended')
+        quit()
