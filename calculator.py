dc199b0911e93a5941d1d30a143a3337f7b907e7
Adeel Ahmed <adeel0754@gmail.com>
2023-02-09 19:53:55 +0000

    add multiply function

--- calculator.py
+++ calculator.py
@@ -2,11 +2,18 @@ def add (a, b):
     answer = a + b
     print(str(a) + " + ", str(b), " = " + str(answer))
 
-add (5, 7)
+#add (5, 7)
 
 
 def minus (a, b):
     answer = a - b
     print(str(a) + " - ", str(b), " = " + str(answer))
 
-minus (12, 2)
+#minus (12, 2)
+
+
+def multiply(a, b):
+    answer = a * b
+    print(str(a) + " x ", str(b), " = " + str(answer))
+
+multiply (10, 10)
