cd499fcf804d1eac74bf40c5b4dacfb0e0f2c61e
Adeel Ahmed <adeel0754@gmail.com>
2023-03-23 15:12:18 +0000

    get highest quality

--- youtubeDownload.py
+++ youtubeDownload.py
@@ -1,6 +1,12 @@
-from pytube import Youtube
+from pytube import YouTube
 
 url = input("Enter the Youtube link: ")
 
 video = YouTube(url)
 
+stream = video.streams.first()
+
+print("Downloading....")
+stream.download()
+
+print("Video downloaded successfully!")
