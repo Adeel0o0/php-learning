e38f69be33796602aa2925a872141256cc761ae3
Adeel Ahmed <adeel0754@gmail.com>
2023-03-28 11:59:22 +0100

    use library to simplify

--- wordDictionary.py
+++ wordDictionary.py
@@ -1,18 +1,10 @@
+from PyDictionary import PyDictionary
 
+dictionary = PyDictionary()
 
-def main():
-    word_dictionary = {
-        "Hi": "a way of greeting",
-        "Eyes": "An organ used for seeing",
-        "Earth": "A planet in space",
-    }
-   
-    while True:
-     word = input("Enter a word: ")
-     if word == "":
-         break
-     if word in word_dictionary:
-         print(word, ":", word_dictionary[word])
+while True:
+    word = input("Enter a word: ")
+    if word == "":
+        break
+    print(dictionary.meaning(word))
 
-
-main()
